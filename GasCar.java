97cc2557ab0c624231fcd8450ee044e86ef1b44b
Nishtha Sharma <158241584+nishthasharma0@users.noreply.github.com>
2024-02-26 20:08:54 -0800

    Lab 4.3

---
+++ GasCar.java
@@ -0,0 +1,18 @@
+package edu.sdccd.cisc191;
+
+public class GasCar extends Car implements Combustible{
+    private int fuelLevel;
+
+    public GasCar(String manufacturerName, int milesOnVehicle, int price, int numberOfSeats, Option[] option, int numberOfDoors) {
+        super(manufacturerName, milesOnVehicle, price, numberOfSeats, option, numberOfDoors);
+    }
+    public int getNumberOfDoors(){
+        return super.getNumberOfDoors();
+    }
+    public int getFuelLevel(){
+        return fuelLevel;
+    }
+    public void tankUp(){
+        fuelLevel = 100;
+    }
+}
