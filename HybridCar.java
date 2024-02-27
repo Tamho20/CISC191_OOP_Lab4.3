97c1beb3cb91a3af815caf959996ebc7847892f9
Tamho20 <158240730+Tamho20@users.noreply.github.com>
2024-02-26 20:21:44 -0800

    Add files via upload
    Fixed code

---
+++ HybridCar.java
@@ -0,0 +1,30 @@
+package edu.sdccd.cisc191;
+
+public class HybridCar extends Car implements Combustible, Chargeable{
+    private int fuelLevel;
+    private int batteryCharge;
+
+    public HybridCar(String manufacturerName, int milesOnVehicle, int price, int numberOfSeats, Option[] option, int numberOfDoors) {
+        super(manufacturerName, milesOnVehicle, price, numberOfSeats, option, numberOfDoors);
+    }
+
+    public int getNumberOfDoors() {
+        return super.getNumberOfDoors();
+    }
+
+    public int getBatteryCharge() {
+        return batteryCharge;
+    }
+
+    public void chargeUp() {
+        batteryCharge = 100;
+    }
+
+    public int getFuelLevel() {
+        return fuelLevel;
+    }
+
+    public void tankUp() {
+        fuelLevel = 100;
+    }
+}
