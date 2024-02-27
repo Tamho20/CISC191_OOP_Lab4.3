f8710dc87b0248ae5e75f1af83f6a38c9d84902a
Tamho20 <158240730+Tamho20@users.noreply.github.com>
2024-02-26 19:47:04 -0800

    Add files via upload

---
+++ PickupTruck.java
@@ -0,0 +1,14 @@
+package edu.sdccd.cisc191;
+
+public class PickupTruck {
+    private int cargoCapacity;
+
+    public PickupTruck(String manufacturerName, int milesOnVehicle, int price, int numberOfSeats, Option[] option, int cargoCapacity) {
+        super(manufacturerName, milesOnVehicle, price, numberOfSeats, option);
+        this.cargoCapacity = cargoCapacity;
+    }
+
+    public int getCargoCapacity() {
+        return this.cargoCapacity;
+    }
+}
